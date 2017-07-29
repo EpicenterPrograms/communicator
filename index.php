32fb6638365660ab74d42e5966dc242fb4892586
Robert <ambitiousbusinessowner@gmail.com>
2017-07-28 21:39:23 -0500

    made the beginning of the communicator script

---
+++ index.php
@@ -0,0 +1,7 @@
+header("Access-Control-Allow-Origin: *");  // If multiple headers of this type are set, an error might be thrown.
+/*
+if (in_array($_SERVER['HTTP_ORIGIN'], array())) {  // if the request is coming from an acceptable origin (contained within the array)
+    header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+}
+*/
+echo "This is a test.";
