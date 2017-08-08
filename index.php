5393221459a378b0b8c4905831795e2569a20746
Robert <ambitiousbusinessowner@gmail.com>
2017-08-08 18:34:04 -0500

    set up the ability to verify users

--- index.php
+++ index.php
@@ -22,36 +22,105 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     }
     echo "You tried to get something.";
 } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
+    /*
+    POST options:
+    username => 
+    password => 
+    location => 
+    pwd_path => 
+    verification =>
+    verifier => 
+    headers => 
+    action =>
+        verify
+        store
+        recall
+        forget
+        register
+    information => 
+    //// override => 
+    */
     $username = tame($_POST["username"]);
     $password = tame($_POST["password"]);
-    $location = tame($_POST["location"]);
-    if (true) {  //// password_verify($password, file_get_contents("gs://" . $location . "/" . $username . "/password"))) {  // if the password is correct
+    $location = tame($_POST["location"]);  //// You might want to make setting the location easier.
+    if ($_POST["pwd_path"]) {
+        $pwd_path = tame($_POST["pwd_path"]);
+    } else {
+        $pwd_path = "gs://" . strstr($location, "://") . "/" . $username . "/password";
+    }
+    if (tame($_POST["verification"]) === "external") {  // if the user needs to be verified through an external source
+        //// set header
+        if ($_POST["verifier"]) {
+            $verifier = tame($_POST["verifier"]);
+        } else {
+            $verifier = "https://epicenterresources.appspot.com";
+        }
+        $options = array("http" => array(
+            "method" => "POST",
+            "content" => http_build_query(
+                array("action" => "verify", "username" => $username, "password" => $password, "pwd_path" => $pwd_path)
+            )
+        ));
+        if ($_POST["headers"]) {
+            $options["http"]["header"] = $_POST["headers"];
+        }
+        $context = stream_contex_create($options);
+        $destination = fopen($verifier, "r", false, $context);
+        if (!$destination) {  // if the URL doesn't go anywhere
+            //// throw new Exception("Problem with $url, $php_errormsg");
+        }
+        $verified = stream_get_contents($destination);
+        if ($verified === false) {  // if there's a problem reading the data
+            //// throw new Exception("Problem reading data from $url, $php_errormsg");
+        } elseif ($verified === "false") {  // if the username and/or password aren't correct
+            
+        }
+    } elseif (tame($_POST["action"]) === "verify") {  // if the script is just being run to verify a user
+        //// set header
+        if (password_verify($password, file_get_contents($pwd_path))) {
+            $response = true;
+        } else {
+            $response = false;
+        }
+    } else {
+        //// set header?
+        $verified = password_verify($password, file_get_contents($pwd_path));
+        if (!$verified) {
+            
+        }
+    }
+    $verified = true;  ////
+    if ($verified === true || $verified === "true") {  // if the password is correct
         header("Access-Control-Allow-Origin: " . tame($_SERVER["HTTP_ORIGIN"]));
         switch (tame($_POST["action"])) {  // switch uses ==
             case "store":
-                file_put_contents("gs://" . $location, $_POST["information"]);  // not taming the information could be bad
+                //// Make sure people don't write to their password.
+                file_put_contents($location, $_POST["information"]);  // not taming the information could be bad
                 $response = "You wrote to " . $location;
                 break;
             case "recall":
-                $response = file_get_contents("gs://" . $location);
+                $response = file_get_contents($location);
                 //// CloudStorageTools::serve("gs://bucket/file");
                 break;
             case "forget":
-                unlink("gs://" . $location);
+                //// Make sure people don't delete their password (usually).
+                unlink($location);
                 $response = "You deleted " . $location;
                 break;
             default:
-                trigger_error("The action requested is not availible.", E_USER_ERROR);
+                throw new Exception("The action requested is not availible.");
+                //// trigger_error("The action requested is not availible.", E_USER_ERROR);
                 // There's also E_USER_NOTICE (default) and E_USER_WARNING.
         }
-    } elseif (tame($_POST["action"]) === "signup") {
-        //// file_put_contents("gs://" . $location . "/" . $username . "/password", password_hash($password, PASSWORD_DEFAULT));
-    } else {
+    } elseif (tame($_POST["action"]) === "register") {
+        //// file_put_contents($location, password_hash($password, PASSWORD_DEFAULT));
+        $response = "You tried to register.";
+    } elseif (tame($_POST["action"]) !== "verify") {
         $response = "The password is incorrect.";
     }
     echo $response;
 }
-// for deploying this app using Google Cloud Shell:
+// for deploying this app using Google Cloud Shell (when you call the file "communicator"):
 # git clone https://github.com/EpicenterPrograms/communicator communicator && cd communicator && gcloud app deploy && cd ..
 # rm -rf communicator
 ?>
