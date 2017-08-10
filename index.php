e1f090a4c92854c0505e9f37f1684f4d5c19938d
Robert <ambitiousbusinessowner@gmail.com>
2017-08-10 00:55:39 -0500

    implemented verification

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-type");
 
-# use google\appengine\api\cloud_storage\CloudStorageTools;
 
 function tame($information) {
     /**
@@ -13,6 +12,7 @@ function tame($information) {
     $information = htmlspecialchars($information);
     return $information;
 }
+
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
     if (true) {  // if the resource allows any origin
@@ -49,15 +49,16 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     if ($_POST["pwd_path"]) {  // if the path to the password is specified
         $pwd_path = tame($_POST["pwd_path"]);
     } else {
-        $pwd_path = "gs://" . substr($location, strpos($location,"://")+3, strrpos($location,"/")-strlen($location)) . "/password";
+        $pwd_path = "gs://" . substr($location, strpos($location,"://")+3, strrpos($location,"/")-strlen($location)) . "/security/password";
     }
+    $response = array("messages" => array(), "errors" => array());
     if (tame($_POST["verification"]) === "external") {  // if the user needs to be verified through an external source
         if ($_POST["verifier"]) {
             $verifier = tame($_POST["verifier"]);
         } else {
             $verifier = "https://epicenterresources.appspot.com";
             if (!$_POST["pwd_path"]) {
-                $pwd_path = "gs://epicenterresources.appspot.com/users/" . $username . "/password";
+                $pwd_path = "gs://epicenterresources.appspot.com/users/" . $username . "/security/password";
             }
         }
         $options = array("http" => array(
@@ -72,55 +73,58 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
         $context = stream_contex_create($options);
         $destination = fopen($verifier, "r", false, $context);
         if (!$destination) {  // if the URL doesn't go anywhere
-            //// throw new Exception("Problem with $url, $php_errormsg");
+            array_push($response->errors, "There's something wrong with URL for password verification.");
         }
-        $verified = stream_get_contents($destination);
-        if ($verified === false) {  // if there's a problem reading the data
-            //// throw new Exception("Problem reading data from $url, $php_errormsg");
-        } elseif ($verified === "false") {  // if the username and/or password aren't correct
-            
+        $verifier_response = stream_get_contents($destination);
+        if ($verifier_response === false) {  // if there's a problem reading the data
+            array_push($response->errors, "The data from the verifying URL can't be read.");
+        } else {
+            parse_str($verifier_response, $external_verifier);  // undoes http_build_query()
+            $response->verified = $external_verifier->value;
         }
     } elseif (tame($_POST["action"]) === "verify") {  // if the script is just being run to verify a user
         if (password_verify($password, file_get_contents($pwd_path))) {
-            $response = true;
+            $response->value = true;
         } else {
-            $response = false;
+            $response->value = false;
         }
     } else {
-        $verified = password_verify($password, file_get_contents($pwd_path));
-        if (!$verified) {
-            
-        }
+        $response->verified = password_verify($password, file_get_contents($pwd_path));
     }
-    $verified = true;  ////
-    if ($verified === true || $verified === "true") {  // if the password is correct
+    if ($response->verified === true || $response->verified === "true") {  // if the password is correct
         switch (tame($_POST["action"])) {  // switch uses ==
             case "store":
                 //// Make sure people don't write to their password.
                 file_put_contents($location, $_POST["information"]);  // not taming the information could be bad
-                $response = "You wrote to " . $location;
+                array_push($response->messages, "You wrote to " . $location);
                 break;
             case "recall":
-                $response = file_get_contents($location);
-                //// CloudStorageTools::serve("gs://bucket/file");
+                $response->value = file_get_contents($location);
+                array_push($response->messages, "You read from " . $location);
                 break;
             case "forget":
                 //// Make sure people don't delete their password (usually).
                 unlink($location);
-                $response = "You deleted " . $location;
+                array_push($response->messages, "You deleted " . $location);
+                break;
+            case "register":
+                array_push($response->messages, "You're already registered.");
                 break;
             default:
-                throw new Exception("The action requested is not availible.");
-                //// trigger_error("The action requested is not availible.", E_USER_ERROR);
-                // There's also E_USER_NOTICE (default) and E_USER_WARNING.
+                array_push($response->errors, "The action requested is not availible.");
         }
     } elseif (tame($_POST["action"]) === "register") {
-        //// file_put_contents($location, password_hash($password, PASSWORD_DEFAULT));
-        $response = "You tried to register.";
+        file_put_contents($pwd_path, password_hash($password, PASSWORD_DEFAULT));
+        array_push($response->messages, 'You registered the password "' . $password . '" in ' . $pwd_path);
     } elseif (tame($_POST["action"]) !== "verify") {
-        $response = "The password is incorrect.";
+        array_push($response->messages, "The username and/or password isn't correct.");
     }
-    echo $response;
+    if (tame($_POST["action"]) === "verify") {
+        echo http_build_query($response);
+        /// Verification responses always return strings, so $response should be encoded in an easily decodable form.
+    } else {
+        echo $response;
+    }
 }
 // for deploying this app using Google Cloud Shell (when you call the file "communicator"):
 # rm -rf communicator && git clone https://github.com/EpicenterPrograms/communicator communicator && cd communicator && gcloud app deploy && cd ..
